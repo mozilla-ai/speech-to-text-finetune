f588f5e07d629f37a8e3bbde398836cb86b10005
Kostis-S-Z <kostissz@pm.me>
2025-02-17 11:41:18 +0200

    Define path in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,10 @@ import pytest
 @pytest.fixture(scope="session")
 def example_data():
     return str(Path(__file__).parent.parent / "example_data/custom")
+
+
+@pytest.fixture(scope="session")
+def language_file_path():
+    return str(
+        Path(__file__).parent.parent / "example_data/languages_common_voice_17_0.json"
+    )
--- tests/unit/test_finetune_whisper.py
+++ tests/unit/test_finetune_whisper.py
@@ -2,20 +2,18 @@ import pytest
 
 from speech_to_text_finetune.finetune_whisper import get_language_id_from_language_name
 
-default_lang_path = "example_data/languages_common_voice_17_0.json"
 
-
-def test_get_language_id_from_language_name():
-    language_id = get_language_id_from_language_name("Abkhaz", default_lang_path)
+def test_get_language_id_from_language_name(language_file_path):
+    language_id = get_language_id_from_language_name("Abkhaz", language_file_path)
     assert language_id == "ab"
 
-    language_id = get_language_id_from_language_name("Zulu", default_lang_path)
+    language_id = get_language_id_from_language_name("Zulu", language_file_path)
     assert language_id == "zu"
 
 
-def test_get_language_id_from_language_name_unlisted():
+def test_get_language_id_from_language_name_unlisted(language_file_path):
     with pytest.raises(KeyError):
-        get_language_id_from_language_name("Unlisted language", default_lang_path)
+        get_language_id_from_language_name("Unlisted language", language_file_path)
 
 
 def test_get_language_id_from_language_name_missing_lang_file():
