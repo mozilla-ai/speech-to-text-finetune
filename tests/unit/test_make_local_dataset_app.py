d6cdbc6eea5a53dd71f2a2b4cec86922b48ac6c9
Kostis-S-Z <kostissz@pm.me>
2025-02-17 11:34:34 +0200

    Use tmp_path

--- tests/unit/test_make_local_dataset_app.py
+++ tests/unit/test_make_local_dataset_app.py
@@ -1,4 +1,3 @@
-import shutil
 from pathlib import Path
 from typing import cast
 
@@ -9,17 +8,6 @@ import pytest
 from speech_to_text_finetune.make_local_dataset_app import save_text_audio_to_file
 
 
-tmp_dir = "tmp"
-
-
-@pytest.fixture(autouse=True)
-def setup_and_teardown():
-    if Path(tmp_dir).is_dir():
-        shutil.rmtree(tmp_dir)
-    yield  # run test
-    shutil.rmtree(tmp_dir)
-
-
 @pytest.fixture
 def dummy_audio_input():
     dummy_sample_rate = 1000
@@ -27,37 +15,37 @@ def dummy_audio_input():
     return cast(gradio.Audio, (dummy_sample_rate, dummy_audio_data))
 
 
-def test_save_text_audio_to_file_new_file(dummy_audio_input):
+def test_save_text_audio_to_file_new_file(tmp_path, dummy_audio_input):
     test_sentence = "test"
     status, none = save_text_audio_to_file(
-        audio_input=dummy_audio_input, sentence=test_sentence, dataset_dir=tmp_dir
+        audio_input=dummy_audio_input, sentence=test_sentence, dataset_dir=tmp_path
     )
 
-    result_df = pd.read_csv(f"{tmp_dir}/text.csv")
-    assert Path(f"{tmp_dir}/rec_0.wav").is_file()
+    result_df = pd.read_csv(f"{tmp_path}/text.csv")
+    assert Path(f"{tmp_path}/rec_0.wav").is_file()
     assert result_df["sentence"][0] == test_sentence
     assert result_df["index"][0] == 0
     assert (
         status
-        == f"✅ Updated {tmp_dir}/text.csv \n✅ Saved recording to {tmp_dir}/rec_0.wav"
+        == f"✅ Updated {tmp_path}/text.csv \n✅ Saved recording to {tmp_path}/rec_0.wav"
     )
     assert none is None
 
 
-def test_save_text_audio_to_file_append_to_file(dummy_audio_input):
+def test_save_text_audio_to_file_append_to_file(tmp_path, dummy_audio_input):
     test_sentence_2 = "test_2"
     save_text_audio_to_file(
-        audio_input=dummy_audio_input, sentence="test_1", dataset_dir=tmp_dir
+        audio_input=dummy_audio_input, sentence="test_1", dataset_dir=tmp_path
     )
     status, none = save_text_audio_to_file(
-        audio_input=dummy_audio_input, sentence=test_sentence_2, dataset_dir=tmp_dir
+        audio_input=dummy_audio_input, sentence=test_sentence_2, dataset_dir=tmp_path
     )
 
-    result_df = pd.read_csv(f"{tmp_dir}/text.csv")
+    result_df = pd.read_csv(f"{tmp_path}/text.csv")
     assert result_df["sentence"][1] == test_sentence_2
-    assert Path(f"{tmp_dir}/rec_1.wav").is_file()
+    assert Path(f"{tmp_path}/rec_1.wav").is_file()
     assert (
         status
-        == f"✅ Updated {tmp_dir}/text.csv \n✅ Saved recording to {tmp_dir}/rec_1.wav"
+        == f"✅ Updated {tmp_path}/text.csv \n✅ Saved recording to {tmp_path}/rec_1.wav"
     )
     assert none is None
