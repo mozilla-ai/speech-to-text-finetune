d92fca112c7bf8d8341c3184a9c8f01ce88cf221
Kostis-S-Z <kostissz@pm.me>
2025-04-03 18:51:43 +0200

    Update existing HF model card with FLEURS

--- src/speech_to_text_finetune/utils.py
+++ src/speech_to_text_finetune/utils.py
@@ -138,3 +138,25 @@ This model was created from the Mozilla.ai Blueprint:
 """
 
     return ModelCard(content)
+
+
+def update_hf_model_card_with_fleurs_results(
+    model_repo_id: str,
+    language: str,
+    ft_eval_results: Dict,
+) -> None:
+    """
+    Update the HF Model Card with the evaluation results from the FLEURS dataset.
+    """
+    model_card = ModelCard.load(model_repo_id)
+    model_card.update(
+        f"""
+### Finetuned model (after finetuning) on the {language} FLEURS test set (total of {ft_eval_results["n_eval_samples"]} samples)
+- Word Error Rate (Normalized): {round(ft_eval_results["eval_wer"], 3)}
+- Word Error Rate (Orthographic): {round(ft_eval_results["eval_wer_ortho"], 3)}
+- Character Error Rate (Normalized): {round(ft_eval_results["eval_cer"], 3)}
+- Character Error Rate (Orthographic): {round(ft_eval_results["eval_cer_ortho"], 3)}
+- Loss: {round(ft_eval_results["eval_loss"], 3)}
+"""
+    )
+    model_card.push_to_hub(model_repo_id)
